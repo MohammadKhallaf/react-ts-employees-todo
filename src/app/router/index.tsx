bd0897fc682fed64473a5b329794e6f3d94068f7
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-11-16 07:24:44 +0200

    add register page

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../../App";
 import LoginPage from "../../pages/LoginPage";
+import RegisterPage from "../../pages/RegisterPage";
 import TasksPage from "../../pages/TasksPage";
 import UsersPage from "../../pages/UsersPage";
 export const router = createBrowserRouter([
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
       {
         path: "/login",
         element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
       },
       {
         path: "users",
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,138 @@
+import React, { FormEvent } from "react";
+import { supabase, useRegister } from "../app/services/api";
+
+type Props = {};
+
+const RegisterPage = (props: Props) => {
+  const register = useRegister();
+
+  const registerHandler = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      email: { value: string };
+      password: { value: string };
+      first_name: { value: string };
+      last_name: { value: string };
+    };
+    const email = target.email.value;
+    const password = target.password.value;
+    const first_name = target.first_name.value;
+    const last_name = target.last_name.value;
+    const { data, error } = await register(
+      email,
+      password,
+      first_name,
+      last_name
+    );
+    console.log(data);
+  };
+  return (
+    <div className="container mx-auto ">
+      <div className="p-8 min-w-[30rem] lg:w-1/3 mx-auto ">
+        <div className=" rounded-b-lg py-12 px-4 lg:px-24  bg-slate-200">
+          <h3 className="text-center text-3xl font-semibold text-gray-900">
+            Register
+          </h3>
+          <form className="mt-6" onSubmit={registerHandler}>
+            <div className="flex flex-col md:flex-row gap-3 ">
+              <div className="relative flex-grow">
+                <input
+                  className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+                  id="first_name"
+                  type="text"
+                  placeholder="First Name"
+                  autoComplete="new-username"
+                />
+                <div className="absolute left-0 inset-y-0 flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-7 w-7 ml-3 text-gray-400 p-1"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="relative flex-grow">
+                <input
+                  className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+                  id="last_name"
+                  type="text"
+                  placeholder="Last Name"
+                  autoComplete="new-username"
+                />
+                <div className="absolute left-0 inset-y-0 flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-7 w-7 ml-3 text-gray-400 p-1"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+
+            <div className="relative mt-3">
+              <input
+                className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+              />
+              <div className="absolute left-0 inset-y-0 flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 ml-3 text-gray-400 p-1"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                </svg>
+              </div>
+            </div>
+            <div className="relative mt-3">
+              <input
+                className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+                autoComplete="new-password"
+              />
+              <div className="absolute left-0 inset-y-0 flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 ml-3 text-gray-400 p-1"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
+                </svg>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center mt-8">
+              <button className="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
