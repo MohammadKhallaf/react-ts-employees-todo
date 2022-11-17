58419500a8d342501bbfc9c3dacc3f202ac5ecf4
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-11-17 07:37:23 +0200

    add user profile to user state

--- src/app/features/userSlice.ts
+++ src/app/features/userSlice.ts
@@ -21,16 +21,32 @@ const initialState: User = {
 };
 
 // Read All Tasks
-export const getUserProfile = createAsyncThunk("user/profile", async () => {
+export const getUserProfile = createAsyncThunk(
+  "user/profile",
+  async (_, thunkAPI) => {
+    const { data: user } = await supabase.auth.getUser();
+    const { data: profile } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", user.user?.id)
+      .limit(1)
+      .single();
+    if (profile.is_admin) {
+      thunkAPI.dispatch(getAllUsers());
+    }
+    return profile;
+  }
+);
+
+export const getAllUsers = createAsyncThunk("user/list", async () => {
   const { data: user } = await supabase.auth.getUser();
-  const { data: profile } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("id", user.user?.id)
-    .limit(1)
-    .single();
-  console.log(profile);
-  return profile;
+  let { data: profiles, error } = await supabase.from("profiles").select("*");
+  return profiles;
+});
+
+export const logOutUser = createAsyncThunk("user/logout", async () => {
+  const { error } = await supabase.auth.signOut();
+  if (!error) return true;
 });
 
 export const userSlice = createSlice({
@@ -43,6 +59,13 @@ export const userSlice = createSlice({
         ...state,
         ...payload,
       };
+    });
+    builder.addCase(getAllUsers.fulfilled, (state, { payload }) => {
+      if (payload) state.users = payload;
+    });
+    builder.addCase(logOutUser.fulfilled, (state, { payload }) => {
+      if (payload) state = initialState;
+      return state;
     });
   },
 });
