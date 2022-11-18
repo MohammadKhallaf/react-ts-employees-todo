2652d27d7e30d472389bd3ecc3166409e34dbbc5
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-11-18 12:54:07 +0200

    remove onClick from invite button

--- src/components/user/UserForm.tsx
+++ src/components/user/UserForm.tsx
@@ -102,7 +102,6 @@ const UserForm = () => {
 
       <button
         className="mt-5 self-end whitespace-nowrap rounded border-0 bg-indigo-500 py-2 px-8 text-lg text-white hover:bg-indigo-600 focus:outline-none"
-        onClick={inviteUserHandler}
         type="submit"
       >
         Invite User
