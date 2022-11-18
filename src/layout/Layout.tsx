aebfd93b792a4f9b5fe481fd7d4dc1ea790dd08e
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-11-18 08:41:06 +0200

    add footer

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { ToastContainer } from "react-toastify";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import Navbar from "./Navbar";
 
 type Props = {
@@ -11,8 +10,42 @@ const Layout = (props: Props) => {
     <>
       <Navbar />
       <main className="container mx-auto ">{props.children}</main>
-      {/* <footer>Footer</footer> */}
-     
+      <footer className="mt-auto">
+        <div className="bg-gray-100">
+          <div className="container mx-auto flex flex-col flex-wrap py-4 px-5 sm:flex-row">
+            <p className="text-center text-sm text-gray-500 sm:text-left">
+              © {new Date().getFullYear()} EmTasks —
+              <a
+                href="https://www.linkedin.com/in/mohammedkhallaf/"
+                rel="noopener noreferrer"
+                className="ml-1 text-gray-600"
+                target="_blank"
+              >
+                @mohammedkhallaf
+              </a>
+            </p>
+            <span className="mt-2 inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start">
+              <a
+                href="https://github.com/MohammadKhallaf"
+                rel="noopener noreferrer"
+                className="ml-1 text-gray-600"
+                target="_blank"
+              >
+                <GitHubLogoIcon className="icon" />
+              </a>
+
+              <a
+                href="https://www.linkedin.com/in/mohammedkhallaf/"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="ml-3 text-gray-600"
+              >
+                <LinkedInLogoIcon className="icon" />
+              </a>
+            </span>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
