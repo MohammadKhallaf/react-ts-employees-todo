77130e2492f84c6754aaf901854a4e29d6e9fcc6
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-11-18 08:41:28 +0200

    styling update

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -1,4 +1,3 @@
-import { ListBulletIcon } from "@radix-ui/react-icons";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -11,16 +10,14 @@ type Props = {
 
 const ContentCard = ({ to, title, content, Icon }: Props) => {
   return (
-    <div className="xl:w-1/3 md:w-1/2 p-4 ">
+    <div className="w-full p-4 md:w-1/2 xl:w-1/4">
       <Link to={to}>
-        <div className="border border-gray-200 p-6 rounded-lg hover:shadow-md">
-          <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+        <div className="rounded-lg border border-gray-200 p-6 shadow hover:shadow-md">
+          <div className=" mb-4 inline-flex h-8 w-full items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
             {Icon}
           </div>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-            {title}
-          </h2>
-          <p className="leading-relaxed text-base">{content}</p>
+          <h2 className="mb-2 text-lg font-medium text-gray-900">{title}</h2>
+          <p className="text-base leading-relaxed">{content}</p>
         </div>
       </Link>
     </div>
--- src/components/routers/AuthRoutes.tsx
+++ src/components/routers/AuthRoutes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useAppSelector } from "../../app/store";
 
@@ -9,9 +9,10 @@ type Props = {
 
 const AuthRoutes = (props: Props) => {
   const user_id = useAppSelector((state) => state.user.id);
+  const location = useLocation();
   if (!user_id) {
     toast.error("You are not logged in!");
-    return <Navigate to="/login" />;
+    return <Navigate to={"/login"} />;
   } else {
     return <>{props.children}</>;
   }
